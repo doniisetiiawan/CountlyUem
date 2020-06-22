9f91c5fb1c0fcef0a44e282bd365725e2bbe4fc0
Doni Setiawan <donisetiawan.050@gmail.com>
2020-06-22 15:26:11 +0700

    Container and presentational components

---
+++ src/Counter.jsx
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  appName: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  tally: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 20,
+    fontSize: 25,
+  },
+  button: {
+    backgroundColor: 'blue',
+    width: 100,
+    marginBottom: 20,
+    padding: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+});
+
+const Counter = (props) => (
+  <View style={styles.container}>
+    <Text style={styles.appName}>Countly</Text>
+    <Text style={styles.tally}>Tally: {props.count}</Text>
+    <TouchableOpacity
+      onPress={props.increment}
+      style={styles.button}
+    >
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      onPress={props.decrement}
+      style={styles.button}
+    >
+      <Text style={styles.buttonText}>-</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      onPress={props.zero}
+      style={styles.button}
+    >
+      <Text style={styles.buttonText}>0</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+export default Counter;
+
+Counter.propTypes = {
+  count: PropTypes.number,
+  decrement: PropTypes.func,
+  increment: PropTypes.func,
+  zero: PropTypes.func,
+};
+
+Counter.defaultProps = {
+  count: 0,
+  decrement: () => {},
+  increment: () => {},
+  zero: () => {},
+};
--- src/Countly.jsx
+++ src/Countly.jsx
@@ -1,92 +1,22 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { increment, decrement, zero } from './actions';
+import { connect, Provider } from 'react-redux';
+import * as actions from './actions';
 import store from './store';
+import Counter from './Counter';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  appName: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  tally: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 20,
-    fontSize: 25,
-  },
-  button: {
-    backgroundColor: 'blue',
-    width: 100,
-    marginBottom: 20,
-    padding: 20,
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 20,
-  },
+const mapStateToProps = (state) => ({
+  count: state.count,
 });
 
-class Countly extends React.Component {
-  constructor(props) {
-    super(props);
+const CounterContainer = connect(
+  mapStateToProps,
+  actions,
+)(Counter);
 
-    this.state = {
-      tally: store.getState(),
-      unsubscribe: store.subscribe(this.updateState),
-    };
-  }
-
-  componentWillUnmount = () => {
-    this.state.unsubscribe();
-  };
-
-  updateState = () => {
-    this.setState({
-      tally: store.getState(),
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.appName}>Countly</Text>
-        <Text style={styles.tally}>
-          Tally: {this.state.tally.count}
-        </Text>
-        <TouchableOpacity
-          onPress={() => store.dispatch(increment())}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => store.dispatch(decrement())}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>-</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => store.dispatch(zero())}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>0</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+const Countly = () => (
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>
+);
 
 export default Countly;
